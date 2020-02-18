ffc5996d6962f04e39160a7ecb3e2cceace21ca9
Dima Dubovski <dzmitry.duboyski@gmail.com>
2020-02-18 15:42:25 +0300

    feat: complit task(dream-team)

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,13 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(input) {
+  if(Array.isArray(input)){
+    let result = input.filter(a=>(Array.isArray(a)===false))
+    result = input.filter(a=>(typeof(a)==='string'))
+    result = result.map(elem=>elem.trim())
+    result = result.map(elem=>elem[0])
+    result = result.map(elem=>elem.toUpperCase())
+    result = result.sort()
+    result = result.join('') 
+    return result
+  }
+  return false
 };
