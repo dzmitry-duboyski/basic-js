8f5eb6a9a631846ddeb3de71eaa1fb4e362092bc
Dima Dubovski <dzmitry.duboyski@gmail.com>
2020-02-18 14:16:24 +0300

    feat: complited task(carbon-dating)

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,18 @@
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample(input) {
+  if((typeof(input)) === 'string'){
+    input=parseFloat(input)
+    if((typeof(input)) ==='number'){
+      if(input > 0 && input < 15){
+        let numerator = (MODERN_ACTIVITY / input)// 
+        numerator = Math.log(numerator)//верх
+        let res=((numerator / 0.693)*HALF_LIFE_PERIOD)
+        return Math.ceil(res)
+      }
+      return false
+    }
+  } 
+    return false
 };
