ba4a7f549413447581355ea1cd0d574cf71020d7
Dima Dubovski <dzmitry.duboyski@gmail.com>
2020-02-20 18:44:39 +0300

    feat: add task(what-season)

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,31 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  const season = {
+    0: 'winter',
+    1: 'winter',
+    2: 'spring',
+    3: 'spring',
+    4: 'spring',
+    5: 'summer',
+    6: 'summer',
+    7: 'summer',
+    8: 'autumn',
+    9: 'autumn',
+    10: 'autumn',
+    11: 'winter',
+  }
+
+  if (typeof (date) === 'undefined') {
+    return 'Unable to determine the time of year!'
+  }
+  if ((date.getTime()) < 100000000 && (date.getTime()) > -100000000) {
+    return 'Error: time is undefined, input correct time'
+  }
+
+  let result
+  let monthNow = date.getMonth()
+
+  if (monthNow <= 11 && 0 <= monthNow) {
+    result = season[monthNow]
+  }
+  return result
 };
